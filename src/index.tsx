f5686aed4c41ceb5c982df9cc9fa09417f19fe6f
demog <dg08gg@mail.ru>
2022-11-14 10:49:45 +0300

    index.tsx
    Импортировал стили бутстрапа

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {setupStore} from "./store/store";
 import {Provider} from "react-redux";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
