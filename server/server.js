9a7df90b948865a651f4db15dcd3fa4b807a228d
demog <dg08gg@mail.ru>
2022-11-18 21:55:20 +0300

    server.js / корень сервера
    Убрал ненужное.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./router')
-const path = require('path')
 
 const app = express()
 const port = 8000
 
 app.use(cors())
 app.use(express.json({ extended: true }))
-app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use('', router)
 
 app.listen(port, () => {
