6285add01eb6673172841218263617b0542cc965
demog <dg08gg@mail.ru>
2022-11-18 20:50:06 +0300

    control.tsx / управление
    В принципе, 1 часть готова - функцианал. Теперь можно отправлять посты.

--- src/components/control.tsx
+++ src/components/control.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {fourChanAPI} from "../service/FourChanService";
+import {Button, Form} from "react-bootstrap";
+import {FileUploader} from "react-drag-drop-files";
+
+const fileTypes = ['JPG', 'PNG', 'JPEG']
 
 const Control = () => {
+  const [post] = fourChanAPI.usePostMessageMutation()
+  const [author, setAuthor] = useState('')
+  const [message, setMessage] = useState('')
+  const [file, setFile] = useState<File>()
+  const handleChange = (newFile: File) => {
+    setFile(newFile)
+  }
+  const postHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('auhtor', author)
+    formData.append('message', message)
+    if (file) {
+      formData.append('image', file)
+    }
+    console.log(formData)
+    post(formData)
+  }
   return (
     <div>
-      
+      <Form.Control
+        value={author}
+        onChange={(e) => setAuthor(e.currentTarget.value)}
+      />
+      <Form.Control
+        value={message}
+        onChange={(e) => setMessage(e.currentTarget.value)}
+      />
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+      <Button onClick={postHandler}>Post</Button>
     </div>
   );
 };
