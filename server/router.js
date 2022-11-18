b6b7762292fcd990eb9d99007e1e3fcc7f433458
demog <dg08gg@mail.ru>
2022-11-18 21:53:53 +0300

    router.js / роутер
    Теперь добавил настоящий функцианал для создания файлов

--- server/router.js
+++ server/router.js
@@ -1,12 +1,18 @@
 const { Router } = require('express')
 const upload = require('./multerMiddleware')
+const fsHandler = require("./fsHandler");
 const router = Router()
 
 router.post('/upload', upload.single('image'), (req, res) => {
-  const product = req.body;
-  console.log(product)
-  if (req.file) {
-    console.log(req.file)
+  if (req.body.message) {
+    const post = {
+      author: req.body.author ? req.body.author : undefined,
+      message: req.body.message,
+      imagePath: req.file ? req.file.destination : undefined
+    }
+    fsHandler.createPost(post, res)
+  } else {
+    res.status(400).send('Поле "message" обязательно')
   }
 });
 
