741dbdb24daf45d2f5fa84b66df15181ee3f2473
demog <dg08gg@mail.ru>
2022-11-19 21:34:42 +0300

    fsHandler.js / взаимодействие с файлами
    Добавил функцию для взятия инфо о посте

--- server/fsHandler.js
+++ server/fsHandler.js
@@ -3,8 +3,9 @@ const {v4} = require('uuid')
 
 const fsHandler = {
   createPost: (post, res) => {
-    fs.writeFile(`./server/uploads/posts/${v4()}${post.author ? post.author : new Date().toLocaleDateString()}.json`,
-      JSON.stringify(post),
+    const postId = v4()
+    fs.writeFile(`./server/uploads/posts/${postId}${post.author ? post.author : new Date().toLocaleDateString()}.json`,
+      JSON.stringify({...post, id: postId}),
       (err) => {
       if (err) {
         res.status(500).send(err)
@@ -12,6 +13,22 @@ const fsHandler = {
         res.status(200).send('success')
       }
     })
+  },
+  getPosts: (res) => {
+    try {
+      const filesData = []
+      fs.readdir('./server/uploads/posts', async (err, files) => {
+        for (let i = 0; i < files.length; i++) {
+          if (files[i] !== '.gitkeep') {
+            const fileData = await JSON.parse(fs.readFileSync(`./server/uploads/posts/${files[i]}`).toString())
+            filesData.push(fileData)
+          }
+        }
+        res.status(200).send(filesData)
+      })
+    } catch (e) {
+      res.status(500).send(e)
+    }
   }
 }
 
