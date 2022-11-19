30622c63fcc0c5236d71d897eb278d9c67e4c83c
demog <dg08gg@mail.ru>
2022-11-19 22:13:10 +0300

    index.tsx / корень проекта
    Отключил удалённое хранилище redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {setupStore} from "./store/store";
-import {Provider} from "react-redux";
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = setupStore()
-
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 );
