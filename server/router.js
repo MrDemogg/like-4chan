da98e92cdac84e183169b6284ba2ccb5f355c160
demog <dg08gg@mail.ru>
2022-11-19 22:07:03 +0300

    router.js / роутер
    Добавил обработку GET запроса на endpoint 'uploads'

--- server/router.js
+++ server/router.js
@@ -3,18 +3,22 @@ const upload = require('./multerMiddleware')
 const fsHandler = require("./fsHandler");
 const router = Router()
 
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/uploads', upload.single('image'), (req, res) => {
   if (req.body.message) {
     const post = {
       author: req.body.author ? req.body.author : undefined,
       message: req.body.message,
-      imagePath: req.file ? req.file.destination : undefined
+      imagePath: req.file ? req.file.filename : undefined
     }
     fsHandler.createPost(post, res)
   } else {
     res.status(400).send('Поле "message" обязательно')
   }
 });
+
+router.get('/uploads', (_, res) => {
+  fsHandler.getPosts(res)
+})
 
 
 module.exports = router
