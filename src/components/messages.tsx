01256316816e942e767283931c1bbde4f423186a
demog <dg08gg@mail.ru>
2022-11-19 22:04:56 +0300

    messages.tsx / компонент "сообщений"
    Компонент для вывода сообщений / постов. Закончена 1 часть - функцианал

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {fourChanAPI} from "../service/FourChanService";
 
 const Messages = () => {
+  const {data, error, isLoading} = fourChanAPI.useGetPostsQuery(null)
   return (
     <div>
-      
+      {isLoading
+        ? <div>Загрузка...</div>
+        : error
+          ? <div>Неожиданная ошибка :(</div>
+          : data && data.map(post =>
+              <div key={post.id}>
+                {post.author && <div>{post.author}</div>}
+                <div>{post.message}</div>
+                {post.imagePath && <img src={`http://localhost:8000/uploads/images/${post.imagePath}`} alt='Картинка поста' />}
+              </div>
+            )
+      }
     </div>
   );
 };
