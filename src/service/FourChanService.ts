2eea2cd228126b1f0732fbb4ee136f6b93ea007d
demog <dg08gg@mail.ru>
2022-11-19 21:33:24 +0300

    FourChanService.ts / сервис
    Сменил названия, добавил функцию GET запроса с сервера.

--- src/service/FourChanService.ts
+++ src/service/FourChanService.ts
@@ -1,18 +1,25 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react'
-
+import {IGetPosts} from "../models/IGetPosts";
 export const fourChanAPI = createApi({
   reducerPath: 'fourChanAPI',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
   tagTypes: ['Post'],
   endpoints: (build) => ({
-    postMessage: build.mutation<string, FormData>({
+    postRequest: build.mutation<string, FormData>({
       query: (message) => ({
-        url: '/upload',
+        url: '/uploads',
         method: 'POST',
         body: message,
         responseHandler: response => response.text()
       }),
       invalidatesTags: ['Post']
+    }),
+    getPosts: build.query<IGetPosts[], null>({
+      query: () => ({
+        url: '/uploads',
+        method: 'GET'
+      }),
+      providesTags: result => ['Post']
     })
   })
 })
