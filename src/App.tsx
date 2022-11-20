dd5d642307c4d671894f1e0a1ddf8873a3af23d1
demog <dg08gg@mail.ru>
2022-11-20 08:52:59 +0300

    App.tsx
    Поменял местами Messages и Control

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Control from "./components/control";
 import Messages from "./components/messages";
+import {Card} from "react-bootstrap";
 
 const App = () => {
   return (
-    <div>
-      <Control />
+    <Card>
       <Messages />
-    </div>
+      <Control />
+    </Card>
   );
 };
 
